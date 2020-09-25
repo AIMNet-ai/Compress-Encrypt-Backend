ed0419b08597000b0d265ce92c10fbc2b38b1f89
Mayank-MP05 <mayank5pande@gmail.com>
2020-09-25 09:26:48 +0530

    Build Folder Moved to Backend Static

---
+++ build/precache-manifest.55eda12de4d2d673ad2be29bcba62f99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7b7f0b3aad0c990f7c0280ab7074bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c204e37ea58f434b624e",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c204e37ea58f434b624e",
+    "url": "/static/js/2.7d032449.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.7d032449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "740ce26575610f843491",
+    "url": "/static/js/main.399d5a5b.chunk.js"
+  },
+  {
+    "revision": "aaafacec07dc17497ad0",
+    "url": "/static/js/runtime-main.3abf3291.js"
+  }
+]);
